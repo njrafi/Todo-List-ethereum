b89e73e582a63dd14b20b1639ed394ea02b71ff8
Nayeem Jahan Rafi <njrafibd@gmail.com>
2021-05-27 22:02:16 +0600

    Added Task list

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,6 +3,24 @@ pragma solidity ^0.5.0;
 contract TodoList {
     uint public taskCount = 0;
 
-    
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+
+    constructor() public {
+        createTask("I am a disco dancer");
+        createTask("Biri khaile hoy cancer");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount,_content, false);
+    }
+
 
 }
